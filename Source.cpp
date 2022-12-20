f7ab21e25389a9cc29ea6bfb9b39d34184ffef6f
Hisokula <poymanova2004@mail.ru>
2022-12-20 16:20:32 +0200

    comments+class

--- Source.cpp
+++ Source.cpp
@@ -4,97 +4,116 @@
 
 namespace esp
 {
-	void Controls(sf::Sprite* Robert, sf::Texture* robert_texture, sf::Texture* rev_robert_texture, float* CurrentFrame, float* time)
-	{
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+    //оказываетс€, класс нужно объ€вл€ть в заголовочном файле, поэтому что будет здесь € без пон€ти€
+
+
+    /*class PlayerRobert
+    {
+    private:
+        sf::Sprite m_Robert;
+        sf::Texture m_robert_texture;
+        sf::Texture m_rev_robert_texture;
+    public:
+        PlayerRobert(sf::Sprite Robert, sf::Texture robert_texture, sf::Texture rev_robert_texture)
         {
-            (*Robert).setTexture(*rev_robert_texture);
-            (*Robert).setTextureRect(sf::IntRect(0, 0, 256, 256));
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::RShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
-            {
-                (*Robert).move(-0.2 * (*time) / 225, 0);
-                (*CurrentFrame) += 0.05;
-                if ((*CurrentFrame) > 5)
-                    ((*CurrentFrame)) -= 5;
-                (*Robert).setTextureRect(sf::IntRect(256 * int((*CurrentFrame)), 0, 256, 256));
-            }
-            else
-            {
-                (*Robert).move(-0.1 * (*time) / 300, 0);
-                (*CurrentFrame) += 0.03;
-                if ((*CurrentFrame) > 5)
-                    (*CurrentFrame) -= 5;
-                (*Robert).setTextureRect(sf::IntRect(256 * int((*CurrentFrame)), 0, 256, 256));
-            }
-
+            m_Robert = Robert;
+            m_robert_texture = robert_texture;
+            m_rev_robert_texture = rev_robert_texture;
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+
+        void Controls(sf::Sprite* Robert, sf::Texture* robert_texture, sf::Texture* rev_robert_texture, float* CurrentFrame, float* time)
         {
-            (*Robert).setTexture(*robert_texture);
-            (*Robert).setTextureRect(sf::IntRect(0, 0, 256, 256));
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::RShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A))
             {
-                (*Robert).move(0.2 * (*time) / 225, 0);
-                (*CurrentFrame) += 0.05;
-                if ((*CurrentFrame) > 5)
-                    (*CurrentFrame) -= 5;
-                (*Robert).setTextureRect(sf::IntRect(256 * int((*CurrentFrame)), 0, 256, 256));
+                (*Robert).setTexture(*rev_robert_texture);
+                (*Robert).setTextureRect(sf::IntRect(0, 0, 256, 256));
+
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::RShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
+                {
+                    (*Robert).move(-0.2 * (*time) / 225, 0);
+                    (*CurrentFrame) += 0.05;
+                    if ((*CurrentFrame) > 5)
+                        ((*CurrentFrame)) -= 5;
+                    (*Robert).setTextureRect(sf::IntRect(256 * int((*CurrentFrame)), 0, 256, 256));
+                }
+                else
+                {
+                    (*Robert).move(-0.1 * (*time) / 300, 0);
+                    (*CurrentFrame) += 0.03;
+                    if ((*CurrentFrame) > 5)
+                        (*CurrentFrame) -= 5;
+                    (*Robert).setTextureRect(sf::IntRect(256 * int((*CurrentFrame)), 0, 256, 256));
+                }
+
             }
-            else
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
             {
-                (*Robert).move(0.1 * (*time) / 300, 0);
-                (*CurrentFrame) += 0.03;
-                if ((*CurrentFrame) > 5)
-                    (*CurrentFrame) -= 5;
-                (*Robert).setTextureRect(sf::IntRect(256 * int((*CurrentFrame)), 0, 256, 256));
-            }
+                (*Robert).setTexture(*robert_texture);
+                (*Robert).setTextureRect(sf::IntRect(0, 0, 256, 256));
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::RShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
+                {
+                    (*Robert).move(0.2 * (*time) / 225, 0);
+                    (*CurrentFrame) += 0.05;
+                    if ((*CurrentFrame) > 5)
+                        (*CurrentFrame) -= 5;
+                    (*Robert).setTextureRect(sf::IntRect(256 * int((*CurrentFrame)), 0, 256, 256));
+                }
+                else
+                {
+                    (*Robert).move(0.1 * (*time) / 300, 0);
+                    (*CurrentFrame) += 0.03;
+                    if ((*CurrentFrame) > 5)
+                        (*CurrentFrame) -= 5;
+                    (*Robert).setTextureRect(sf::IntRect(256 * int((*CurrentFrame)), 0, 256, 256));
+                }
 
 
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+            {
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::RShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
+                {
+                    (*Robert).move(0, -0.2 * (*time) / 225);
+                    (*CurrentFrame) += 0.05;
+                    if ((*CurrentFrame) > 5)
+                        (*CurrentFrame) -= 5;
+                    (*Robert).setTextureRect(sf::IntRect(256 * int((*CurrentFrame)), 0, 256, 256));
+                }
+                else
+                {
+                    (*Robert).move(0, -0.1 * (*time) / 300);
+                    (*CurrentFrame) += 0.03;
+                    if ((*CurrentFrame) > 5)
+                        (*CurrentFrame) -= 5;
+                    (*Robert).setTextureRect(sf::IntRect(256 * int((*CurrentFrame)), 0, 256, 256));
+                }
+
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+            {
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::RShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
+                {
+                    (*Robert).move(0, 0.2 * (*time) / 225);
+                    (*CurrentFrame) += 0.05;
+                    if ((*CurrentFrame) > 5)
+                        (*CurrentFrame) -= 5;
+                    (*Robert).setTextureRect(sf::IntRect(256 * int((*CurrentFrame)), 0, 256, 256));
+                }
+
+                else
+                {
+                    (*Robert).move(0, 0.1 * (*time) / 300);
+                    (*CurrentFrame) += 0.03;
+                    if ((*CurrentFrame) > 5)
+                        (*CurrentFrame) -= 5;
+                    (*Robert).setTextureRect(sf::IntRect(256 * int((*CurrentFrame)), 0, 256, 256));
+                }
+
+            }
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-        {
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::RShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
-            {
-                (*Robert).move(0, -0.2 * (*time) / 225);
-                (*CurrentFrame) += 0.05;
-                if ((*CurrentFrame) > 5)
-                    (*CurrentFrame) -= 5;
-                (*Robert).setTextureRect(sf::IntRect(256 * int((*CurrentFrame)), 0, 256, 256));
-            }
-            else
-            {
-                (*Robert).move(0, -0.1 * (*time) / 300);
-                (*CurrentFrame) += 0.03;
-                if ((*CurrentFrame) > 5)
-                    (*CurrentFrame) -= 5;
-                (*Robert).setTextureRect(sf::IntRect(256 * int((*CurrentFrame)), 0, 256, 256));
-            }
 
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-        {
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::RShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
-            {
-                (*Robert).move(0, 0.2 * (*time) / 225);
-                (*CurrentFrame) += 0.05;
-                if ((*CurrentFrame) > 5)
-                    (*CurrentFrame) -= 5;
-                (*Robert).setTextureRect(sf::IntRect(256 * int((*CurrentFrame)), 0, 256, 256));
-            }
-
-            else
-            {
-                (*Robert).move(0, 0.1 * (*time) / 300);
-                (*CurrentFrame) += 0.03;
-                if ((*CurrentFrame) > 5)
-                    (*CurrentFrame) -= 5;
-                (*Robert).setTextureRect(sf::IntRect(256 * int((*CurrentFrame)), 0, 256, 256));
-            }
-
-        }
-	}
-    
+    };
+    */
 	
 	
 }
--- main.cpp
+++ main.cpp
@@ -4,30 +4,62 @@
 
 int main()
 {
-
+    //рендер виндова + установка ограничения на фпс
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Nevertheless works!");
     window.setFramerateLimit(120);
 
+
+    //с этим я пока сама не разобралась
+    //возможно, это будет картой :D
+    const int H = 12;
+    const int W = 40;
+
+    sf::String TileMap[H] =
+    {
+        "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+        "B                                      B",
+        "B                                      B",
+        "B                                      B",
+        "B                                      B",
+        "B                                      B",
+        "B                                      B",
+        "BBBB                                   B",
+        "B  BB                                  B",
+        "B   BB                                 B",
+        "BP  XBB                                B",
+        "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+    };
+
+    //сет текстур для Роберта(основной и реверснутой)
     sf::Texture robert_texture;
     robert_texture.loadFromFile("roberto.png");
 
     sf::Texture rev_robert_texture;
     rev_robert_texture.loadFromFile("robertorev.png");
 
+    //создание спрайта Роберта
+    //желательно запихнуть в класс
     sf::Sprite Robert;
     Robert.setTexture(robert_texture);
     Robert.setPosition(960, 540);
     Robert.setTextureRect(sf::IntRect(0, 0, 256, 256));
 
+    //клок клок
     sf::Clock clock;
 
+    sf::RectangleShape rectangle; //борюсь с картой, это к ней относится
+
+    //это важно это надо
     float CurrentFrame = 0;
 
+    //время для виндова
     while (window.isOpen())
     {
+        //тут понятно, снова клок клок
         float time = clock.getElapsedTime().asMicroseconds();
         clock.restart();
 
+        //дабы не создать вечный двигатель
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -35,12 +67,45 @@ int main()
                 window.close();
         }
 
-        window.clear();
+        //черный выглядел стильно, но это тоже ничего
+        window.clear(sf::Color::White);
+
+        
+        //всё ещё борьба с ТаЙлМаПоМ
+        for (int i = 0; i < H; i++)
+        {
+            for (int j = 0; j < W; j++)
+            {
+                if (TileMap[i][j] == 'B')
+                {
+                    rectangle.setFillColor(sf::Color::Black);
+                }
+                if (TileMap[i][j] == 'X')
+                {
+                    rectangle.setFillColor(sf::Color::Magenta);
+                }
+                if (TileMap[i][j] == 'P')
+                {
+                    rectangle.setFillColor(sf::Color::Red);
+                }
+                if (TileMap[i][j] == ' ') continue;
+
+                rectangle.setPosition(j * 32, i * 32);
+                window.draw(rectangle);
+            }
+        }
+
+        //самое вкусное
         window.draw(Robert);
         window.display();
 
+        //я создала рабочий класс, swag
+        //ура, пролетариат
+        esp::PlayerRobert PLAYER(&Robert, &robert_texture, &rev_robert_texture);
+        PLAYER.Controls(&robert_texture, &rev_robert_texture, &CurrentFrame, &time);
         
-        esp::Controls(&Robert, &robert_texture, &rev_robert_texture, &CurrentFrame, &time);
+
+
     }
 
 
